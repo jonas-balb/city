c935cd703dd40e9a9ac839d3ef5b75d782b88324
jonas-balb <jonas.balb@hotmail.com>
2025-02-07 20:24:58 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let Rh = 0
+let soil_moisture = 0
 let ulrasonic_distance = 0
 let Temp = 0
 OLED.init(128, 64)
@@ -7,19 +8,28 @@ basic.forever(function () {
     Temp = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1)
     basic.pause(5000)
     ulrasonic_distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P3)
+    soil_moisture = Environment.ReadSoilHumidity(AnalogPin.P0)
     Rh = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1)
     basic.pause(5000)
     OLED.clear()
-    OLED.writeStringNewLine("Temp" + "Temp")
     OLED.writeStringNewLine("RH" + "RH")
+    OLED.writeStringNewLine("Temp" + "Temp")
+    OLED.writeStringNewLine("soil moistere:" + soil_moisture)
     if (Temp < 14 || Temp > 24) {
         Environment.ledBrightness(AnalogPin.P2, true)
-        if (true) {
-        	
+        Environment.ledBrightness(AnalogPin.P5, false)
+        Environment.ledBrightness(AnalogPin.P4, true)
+        if (ulrasonic_distance > 10 && ulrasonic_distance < 60) {
+            pins.servoWritePin(AnalogPin.P6, 180)
         }
     } else if (Rh < 45 || Rh > 60) {
+        Environment.ledBrightness(AnalogPin.P4, false)
+        Environment.ledBrightness(AnalogPin.P5, true)
         Environment.ledBrightness(AnalogPin.P2, true)
+        pins.servoWritePin(AnalogPin.P6, 180)
     } else {
+        Environment.ledBrightness(AnalogPin.P5, false)
+        Environment.ledBrightness(AnalogPin.P4, false)
         Environment.ledBrightness(AnalogPin.P2, false)
     }
 })
