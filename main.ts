4ddca647b9557fc2597a59d962675e366cbf1b85
jonas-balb <jonas.balb@hotmail.com>
2025-02-08 08:04:38 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 let Rh = 0
 let soil_moisture = 0
 let ulrasonic_distance = 0
+let water_level = 0
 let Temp = 0
 OLED.init(128, 64)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     Temp = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1)
     basic.pause(5000)
+    water_level = Environment.ReadWaterLevel(AnalogPin.P0)
     ulrasonic_distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P3)
-    soil_moisture = Environment.ReadSoilHumidity(AnalogPin.P0)
+    soil_moisture = Environment.ReadSoilHumidity(AnalogPin.P7)
     Rh = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1)
     basic.pause(5000)
     OLED.clear()
@@ -19,6 +21,26 @@ basic.forever(function () {
         Environment.ledBrightness(AnalogPin.P2, true)
         Environment.ledBrightness(AnalogPin.P5, false)
         Environment.ledBrightness(AnalogPin.P4, true)
+        if (water_level > 0) {
+            basic.showLeds(`
+                . # # # .
+                . . # . .
+                . # # # .
+                . . # . .
+                . . # . .
+                `)
+            pins.servoWritePin(AnalogPin.P6, 180)
+        }
+        if (soil_moisture < 60) {
+            basic.showLeds(`
+                . . . . #
+                . . . # #
+                # . # # .
+                # # # . .
+                # # . . .
+                `)
+            pins.servoWritePin(AnalogPin.P6, 90)
+        }
         if (ulrasonic_distance > 10 && ulrasonic_distance < 60) {
             pins.servoWritePin(AnalogPin.P6, 180)
         }
@@ -26,7 +48,7 @@ basic.forever(function () {
         Environment.ledBrightness(AnalogPin.P4, false)
         Environment.ledBrightness(AnalogPin.P5, true)
         Environment.ledBrightness(AnalogPin.P2, true)
-        pins.servoWritePin(AnalogPin.P6, 180)
+        pins.servoWritePin(AnalogPin.P6, 0)
     } else {
         Environment.ledBrightness(AnalogPin.P5, false)
         Environment.ledBrightness(AnalogPin.P4, false)
